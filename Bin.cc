76890ce5e1c04a4ceb4dd26b2596bb9d3df04e75
dingdd <dingdd_email@163.com>
2013-10-29 00:26:52 +0800

    modified Bin.cc
    modified in.cc

--- Bin.cc
+++ Bin.cc
@@ -5,47 +5,37 @@
 using namespace std;
 class BinaryCode
 {
-    public:
-        vector<string> decode(string);
+  public:
+    vector<string> decode(string);
 };
 
 vector<string> BinaryCode::decode(string m){
-    vector<string> codes;
-    int n=m.size(),i;
-    string s1(n+2,'0');
-    string s2(n+2,'0');
-    s2[1]='1';
-    for( i=2;i<n+1;i++){
-        s1[i]=m[i-2]-s1[i-1]+'0'-s1[i-2]+'0';
-        if(s1[i]!='0'&&s1[i]!='1'){
-            codes.push_back("None");
-            break;
-        }
-    }
-    if(s1[i]=='0'||s1[i]=='1'){
-        string s(s1.begin()+1,s1.begin()+n+1);
-        codes.push_back(s);
-    }
-    for(i=2;i<n+1;i++){
-        s2[i]=m[i-2]-s2[i-1]+'0'-s2[i-2]+'0';
-        if(s2[i]!='0'&&s2[i]!='1'){
-            codes.push_back("None");
-            break;
-        }
-    }
-    if(s2[i]=='0'||s2[i]=='1'){
-        string s(s2.begin()+1,s2.begin()+n+1);
-        codes.push_back(s);
-    }
-    return codes;
+  vector<string> codes;
+  int n=m.size();
+	string s(n+2, '0');
+	for(int i=0,j;i<2;i++){
+		s[1]='0'+i;
+		for(j=2;j<n+2;j++){
+			s[j]=m[j-2]-s[j-1]-s[j-2]+'0'+'0';
+			if(s[j]!='0'&&s[j]!='1')
+				break;
+		}
+		if(j==(n+2)&&s[j-1]=='0')
+			codes.push_back(string(s.begin()+1, s.begin()+n+1));
+		else
+			codes.push_back("None");
+	}
+	return codes;
 }
-
+		
 int main(void)
 {
   BinaryCode b;
   vector<string> svec;
-  svec=b.decode("123210120");
-  cout<<svec[0]<<endl;
-  cout<<svec[1]<<endl;
+	string s;
+	while(cin>>s){
+		svec=b.decode(s);
+		cout<<svec[0]<<"\t"<<svec[1]<<endl;
+	}
   return(0);
 }
